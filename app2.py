01ce748bcba7cce7c7681568228cecd1a6fd33c2
Amit Bhramanna <amitbhramanna@gmail.com>
2023-12-26 10:39:42 +0530

    movie recommend

--- app2.py
+++ app2.py
@@ -41,4 +41,4 @@ def process_input():
     return render_template('result.html', data=result,poster_url=poster_url)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
